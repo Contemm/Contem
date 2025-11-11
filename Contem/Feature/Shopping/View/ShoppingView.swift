d6e520882c57292076dbc63f9e72e41ae03056e7
rmfls0606 <rmfls0606@naver.com>
2025-11-11 16:18:41 +0900

    쇼핑화면 파일 추가

---
+++ Contem/Feature/Shopping/View/ShoppingView.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingView.swift
+//  Contem
+//
+//  Created by 이상민 on 11/11/25.
+//
+
+import SwiftUI
+
+struct ShoppingView: View {
+    var body: some View {
+        NavigationStack{
+            Text("ShoppingView")
+        }
+    }
+}
+
+#Preview {
+    ShoppingView()
+}
