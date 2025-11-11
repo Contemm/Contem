0d60d4259b7e934f5800d91e5af5b792a870d6f1
HyoTaek <kht1790@gmail.com>
2025-11-11 19:42:52 +0900

    [Feat] push navigation 메서드 정의
    - push: 특정 페이지로 이동
    - pop: 이전화면으로 돌아가기
    - popToRoot: 루트 화면으로 돌아가기

--- Contem/Coordinator/CoordinatorProtocol.swift
+++ Contem/Coordinator/CoordinatorProtocol.swift
@@ -10,7 +10,20 @@ import SwiftUI
 @MainActor
 protocol CoordinatorProtocol: AnyObject {
     
+    // MARK: - Properties
+    
     var path: NavigationPath { get set }
     var sheet: Page? { get set }
     var fullScreenCover: Page? { get set }
+    
+    // MARK: - Push
+    
+    /// 특정 페이지로 push
+    func push(_ page: Page)
+    
+    /// 이전 화면으로 돌아가기
+    func pop()
+
+    /// 루트 화면으로 돌아가기
+    func popToRoot()
 }
