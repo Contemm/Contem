cebd47ffa696d79efe9ff47950c0fdc67267ee04
rmfls0606 <rmfls0606@naver.com>
2025-11-11 16:18:49 +0900

    피드화면 파일 추가

---
+++ Contem/Feature/Feed/View/FeedView.swift
@@ -0,0 +1,20 @@
+//
+//  FeedView.swift
+//  Contem
+//
+//  Created by 이상민 on 11/11/25.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack{
+            Text("FeedView")
+        }
+    }
+}
+
+#Preview {
+    FeedView()
+}
