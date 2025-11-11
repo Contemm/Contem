b9d91d03d0b1630b76ac6b61f60d8301dc81d6d5
HyoTaek <kht1790@gmail.com>
2025-11-11 19:38:12 +0900

    [Feat] Page enum 정의
    - coordinator에서 사용할 페이지를 열거형으로 정의
    - id는 자기 자신을 사용

---
+++ Contem/Coordinator/Page.swift
@@ -0,0 +1,22 @@
+//
+//  Page.swift
+//  Contem
+//
+//  Created by HyoTaek on 11/11/25.
+//
+
+import Foundation
+
+// TODO: - 화면 많아지면 탭별로 나누기?
+
+/// 앱의 모든 화면을 정의하는 enum
+enum Page: Hashable, Identifiable {
+    
+    // MARK: - Page
+    
+    case main
+    
+    // MARK: - ID
+    
+    var id: Self { self }
+}
