4457127fe8b9d15462faf61c8135a0bc10ec9e3b
HyoTaek <kht1790@gmail.com>
2025-11-11 20:11:40 +0900

    [Feat] Tracking 프로퍼티들 추가
    - pushedPages, presentedSheets, presentedFullScreens: 각각의 방식으로 이동된 페이지들의 기록을 배열로 저장
    - count관련 프로퍼티로 호출 횟수 추적

--- Contem/Coordinator/MockCoordinator.swift
+++ Contem/Coordinator/MockCoordinator.swift
@@ -15,5 +15,25 @@ final class MockCoordinator: ObservableObject, CoordinatorProtocol {
     
     @Published var path = NavigationPath()
     @Published var sheet: Page?
-    @Published var fullScreen: Page?
+    @Published var fullScreenCover: Page?
+    
+    // MARK: - Test Tracking Properties
+    
+    /// Push된 페이지 기록
+    var pushedPages: [Page] = []
+
+    /// Sheet로 표시된 페이지 기록
+    var presentedSheets: [Page] = []
+
+    /// FullScreen으로 표시된 페이지 기록
+    var presentedFullScreens: [Page] = []
+
+    /// 호출 횟수 추적
+    var pushCallCount = 0
+    var popCallCount = 0
+    var popToRootCallCount = 0
+    var presentSheetCallCount = 0
+    var dismissSheetCallCount = 0
+    var presentFullScreenCallCount = 0
+    var dismissFullScreenCallCount = 0
 }
