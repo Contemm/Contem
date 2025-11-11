4bdae03fa47a4c0f72ad89bdd54dc17ee292b85f
HyoTaek <kht1790@gmail.com>
2025-11-11 19:49:41 +0900

    [Feat] Coordinator 생성
    - path, sheet, fullScreenCover 프로퍼티 생성
    - push, pop, popToRoot 메서드 구현

---
+++ Contem/Coordinator/Coordinator.swift
@@ -0,0 +1,37 @@
+//
+//  Coordinator.swift
+//  Contem
+//
+//  Created by HyoTaek on 11/11/25.
+//
+
+import SwiftUI
+import Combine
+
+@MainActor
+final class Coordinator: ObservableObject, CoordinatorProtocol {
+    
+    // MARK: - Properties
+    
+    @Published var path = NavigationPath()
+    @Published var sheet: Page?
+    @Published var fullScreenCover: Page?
+
+    // MARK: - Push
+
+    /// 특정 페이지로 push
+    func push(_ page: Page) {
+        path.append(page)
+    }
+
+    /// 이전 화면으로 돌아가기
+    func pop() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+
+    /// 루트 화면으로 돌아가기
+    func popToRoot() {
+        path = NavigationPath()
+    }
+}
