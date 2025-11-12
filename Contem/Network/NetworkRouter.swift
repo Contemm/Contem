a5d18af7b34c4f6cdcf6e024bfd6ae08ec6249f1
HyoTaek <kht1790@gmail.com>
2025-11-12 19:47:11 +0900

    [Networking] HTTP 메서드 정의

---
+++ Contem/Network/NetworkRouter.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkRouter.swift
+//  Contem
+//
+//  Created by HyoTaek on 11/12/25.
+//
+
+import Foundation
+
+/// HTTP 메서드 정의
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
