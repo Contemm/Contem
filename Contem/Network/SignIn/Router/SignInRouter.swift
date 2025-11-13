0859af0df35d3d0b838753d56bc9b68954e70ff0
HyoTaek <kht1790@gmail.com>
2025-11-13 22:06:32 +0900

    [Networking] 로그인 API 라우터 구현
    - 로그인 post 요청 추가

--- Contem/Network/SignIn/Router/SignInRouter.swift
+++ Contem/Network/SignIn/Router/SignInRouter.swift
@@ -5,4 +5,46 @@
 //  Created by HyoTaek on 11/13/25.
 //
 
-import Foundation
+enum SignInRouter: NetworkRouter {
+    
+    // MARK: - Case
+
+    /// 로그인
+    case signIn(body: [String: String])
+    
+    // MARK: - Path
+    
+    var path: String {
+        switch self {
+        case .signIn:
+            return "users/login"
+        }
+    }
+    
+    // MARK: - Method
+    
+    var method: HTTPMethod {
+        switch self {
+        case .signIn:
+            return .post
+        }
+    }
+    
+    // MARK: - Headers
+    
+    var headers: [String : String]? {
+        return [
+            "SeSACKey": APIConfig.sesacKey,
+            "ProductId": APIConfig.productID
+        ]
+    }
+    
+    // MARK: - Parameters
+    
+    var parameters: [String : Any]? {
+        switch self {
+        case .signIn(let body):
+            return body
+        }
+    }
+}
