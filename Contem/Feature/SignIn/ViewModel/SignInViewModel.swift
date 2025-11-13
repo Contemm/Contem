477d1ce480810e36a5cab769a622e09363c5129f
HyoTaek <kht1790@gmail.com>
2025-11-13 22:50:27 +0900

    [Feat] SignInViewModel 생성

--- Contem/Feature/SignIn/ViewModel/SignInViewModel.swift
+++ Contem/Feature/SignIn/ViewModel/SignInViewModel.swift
@@ -5,4 +5,43 @@
 //  Created by HyoTaek on 11/13/25.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+final class SignInViewModel: ViewModelType {
+    
+    // MARK: - MVVM
+    
+    var cancellables = Set<AnyCancellable>()
+    var input = Input()
+    @Published var output = Output()
+    
+    // MARK: - Dependencies
+
+    private let coordinator: CoordinatorProtocol
+
+    // MARK: - Input
+
+    struct Input {
+        
+    }
+
+    // MARK: - Output
+
+    struct Output {
+        
+    }
+
+    // MARK: - Init
+
+    init(coordinator: CoordinatorProtocol) {
+        self.coordinator = coordinator
+        transform()
+    }
+
+    // MARK: - Transform
+
+    func transform() {
+        
+    }
+}
