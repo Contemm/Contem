8c4b56d549b96c6e2a227a18237f0ba4a9341548
HyoTaek <kht1790@gmail.com>
2025-11-13 22:25:08 +0900

    [UI] 비밀번호 텍스트 필드 추가

--- Contem/Feature/SignIn/SignInView.swift
+++ Contem/Feature/SignIn/SignInView.swift
@@ -12,16 +12,27 @@ struct SignInView: View {
     // MARK: - Property
     
     @State private var email = ""
+    @State private var password = ""
     
     // MARK: - Body
     
     var body: some View {
-        TextField("이메일을 입력해주세요", text: $email)
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: .spacing16)
-                    .stroke(.gray300, lineWidth: 1)
-            )
-            .padding(.horizontal, .spacing16)
+        VStack(spacing: .spacing16) {
+            TextField("이메일을 입력해주세요", text: $email)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: .spacing16)
+                        .stroke(.gray300, lineWidth: 1)
+                )
+                .padding(.horizontal, .spacing16)
+            
+            TextField("비밀번호를 입력해주세요", text: $password)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: .spacing16)
+                        .stroke(.gray300, lineWidth: 1)
+                )
+                .padding(.horizontal, .spacing16)
+        }
     }
 }
