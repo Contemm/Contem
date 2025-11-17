b93550a88b08d28a41c5bb28b822397c0cbe38b3
rmfls0606 <rmfls0606@naver.com>
2025-11-17 11:15:49 +0900

    feat: StyleEntity 및 비율 기반 태그 좌표 모델 추가(#13)

---
+++ Contem/Feature/Style/Model/StyleEntity.swift
@@ -0,0 +1,41 @@
+//
+//  StyleEntity.swift
+//  Contem
+//
+//  Created by 이상민 on 11/16/25.
+//
+
+import Foundation
+
+struct StyleEntity{
+    let postId: String
+    let category: String
+    let title: String
+    let content: String
+    let value1: String
+    let value2: String
+    let value3: String
+    let value4: String
+    let value5: String
+    let createdAt: String
+    let creator: StyleCreatorEntity
+    let files: [String] //이미지들
+    let likes: [String]
+    let commentCount: Int
+    
+    var likeCount: String{
+        likes.count.description
+    }
+}
+
+struct StyleCreatorEntity {
+    let userId: String
+    let nick: String
+    let profileImage: String
+}
+
+struct StyleTag: Identifiable{
+    let id = UUID()
+    let relX: CGFloat //0~1 비율
+    let relY: CGFloat
+}
