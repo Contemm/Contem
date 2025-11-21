0f0e68799587dd9fc9d15e435677f1bb06843d4f
rmfls0606 <rmfls0606@naver.com>
2025-11-22 01:23:40 +0900

    feat: 로그인 응답 모델 DTO 정의(#28)

---
+++ Contem/Shared/Network/Auth/DTO/RefreshDTO.swift
@@ -0,0 +1,13 @@
+//
+//  RefreshDTO.swift
+//  Contem
+//
+//  Created by 이상민 on 11/21/25.
+//
+
+import Foundation
+
+struct RefreshDTO: Codable{
+    let accessToken: String
+    let refreshToken: String
+}
