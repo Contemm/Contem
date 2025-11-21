049427c156cbd79ea342c6c2beb580545403b4f2
rmfls0606 <rmfls0606@naver.com>
2025-11-22 04:38:39 +0900

    feat: API 통신을 통한 로그인 기능 추가(#28)

--- Contem/Feature/SignIn/ViewModel/SignInViewModel.swift
+++ Contem/Feature/SignIn/ViewModel/SignInViewModel.swift
@@ -23,9 +23,9 @@ final class SignInViewModel: ViewModelType {
     }
 
     struct Output {
-        var email = ""
-        var password = ""
-        var shouldShowAlert = false
+        var email: String = ""
+        var password: String = ""
+        var showAlert = false
         var alertMessage = ""
     }
     
@@ -39,87 +39,23 @@ final class SignInViewModel: ViewModelType {
         input.loginButtonTapped
             .withUnretained(self)
             .sink { owner, _ in
-                owner.coordinator?.login()
-//                owner.coordinator?.push(.shoppingDetail)
-//                Task {
-//                    await owner.signIn(body: owner.signInBody)
-//                }
+                Task{
+                    await self.signin()
+                }
             }
             .store(in: &cancellables)
     }
-}
-
-extension SignInViewModel {
-
-    // 로그인 요청 body
-    private var signInBody: [String: String] {
-        return [
-            "email": output.email,
-            "password": output.password
-        ]
+    
+    private func signin() async{
+        do{
+            _ = try await NetworkService.shared.callRequest(
+                router: UserRequest.login(email: output.email, password: output.password),
+                type: LoginDTO.self
+            )
+            coordinator?.rootRoute = .tabView
+        }catch{
+            output.alertMessage = error.localizedDescription
+            output.showAlert = true
+        }
     }
-
-    // 로그인 API 호출
-//    private func signIn(body: [String: String]) async {
-//        do {
-//            let loginResponse = try await signInAPI.signIn(body: body)
-//            let accessToken = loginResponse.accessToken
-//            let refreshToken = loginResponse.refreshToken
-//            
-//            // Keychain에 토큰 저장
-//            do {
-//                try KeychainManager.shared.saveAllTokens(accessToken: accessToken, refreshToken: refreshToken)
-//            } catch {
-//                // Keychain 저장 실패 시 에러 처리
-//                print("Keychain 저장 실패: \(error.localizedDescription)")
-//                output.shouldShowAlert = true
-//                output.alertMessage = "로그인 정보 저장에 실패했습니다.\n앱을 재시작하면 다시 로그인해야 할 수 있습니다."
-//                return
-//            }
-//            
-//            // MainTabView로 화면 전환
-//            appState.signIn()
-////            coordinator?.push(AppCoordinator.Route)
-//        } catch let error as NetworkError {
-//            switch error {
-//            case .badRequest:
-//                output.shouldShowAlert = true
-//                output.alertMessage = "필수 값을 채워주세요"
-//                
-//            case .unauthorized:
-//                output.shouldShowAlert = true
-//                output.alertMessage = "이메일 또는 비밀번호가 일치하지 않습니다"
-//                
-//            case .networkFailure(let underlyingError):
-//                output.shouldShowAlert = true
-//                output.alertMessage = "네트워크 연결을 확인해주세요"
-//
-//            case .timeout:
-//                output.shouldShowAlert = true
-//                output.alertMessage = "네트워크 연결을 확인해주세요"
-//
-//            case .serverError:
-//                output.shouldShowAlert = true
-//                output.alertMessage = "서버에 일시적인 문제가 발생했습니다. 잠시 후 다시 시도해주세요"
-//
-//            case .invalidURL:
-//                print("잘못된 URL 형식 에러: \(error.localizedDescription)")
-//            case .invalidResponse:
-//                print("응답이 HTTPURLResponse가 아닌 경우 에러: \(error.localizedDescription)")
-//            case .forbidden:
-//                print("403 에러: \(error.localizedDescription)")
-//            case .notFound:
-//                print("404 에러: \(error.localizedDescription)")
-//            case .decodingFailed:
-//                print("디코딩 에러: \(error.localizedDescription)")
-//            case .unknownError:
-//                print("알 수 없는 에러: \(error.localizedDescription)")
-//            }
-//
-//        } catch {
-//            // 기타 에러
-//            output.shouldShowAlert = true
-//            output.alertMessage = "알 수 없는 오류가 발생했습니다"
-//        }
-//    }
 }
