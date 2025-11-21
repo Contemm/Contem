c63591b1cee37f90f5217be88853aba838dd4028
rmfls0606 <rmfls0606@naver.com>
2025-11-22 06:03:33 +0900

    refactor: 키체인에 저장된 accessToken 기반 kingfisher Request 작성(#28)

--- Contem/Shared/Utils/ImageRequestModifier.swift
+++ Contem/Shared/Utils/ImageRequestModifier.swift
@@ -11,8 +11,8 @@ import Kingfisher
 struct MyImageDownloadRequestModifier: ImageDownloadRequestModifier {
     func modified(for request: URLRequest) -> URLRequest? {
         var modifiedRequest = request
-        guard let accessToken = try? KeychainManager.shared.read(for: .accessToken) else { return nil }
-        modifiedRequest.setValue(APIConfig.testToken, forHTTPHeaderField: "Authorization")
+        guard let accessToken = try? KeychainManager.shared.read(.accessToken) else { return nil }
+        modifiedRequest.setValue(accessToken, forHTTPHeaderField: "Authorization")
         modifiedRequest.setValue(APIConfig.sesacKey, forHTTPHeaderField: "SeSACKey")
         modifiedRequest.setValue(APIConfig.productID, forHTTPHeaderField: "ProductId")
         return modifiedRequest
