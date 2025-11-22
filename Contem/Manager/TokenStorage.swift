dcd3f621c233a723311c5d04c11e6791746bb9bc
rmfls0606 <rmfls0606@naver.com>
2025-11-23 03:38:53 +0900

    feat: 멀티 스레드 환경에서의 경쟁 상태(Race Conditionn) 방지를 위해 Task를 활용한 리프래시 요청 중복 방지 구현(#28)

--- Contem/Manager/TokenStorage.swift
+++ Contem/Manager/TokenStorage.swift
@@ -11,6 +11,9 @@ final actor TokenStorage {
     static let shared = TokenStorage()
     private init() {}
     
+    //refresh 증복 방지용 Task
+    private var refreshTask: Task<String, Error>? = nil
+    
     func getAccessToken() async -> String? {
         return try? await KeychainManager.shared.read(.accessToken)
     }
@@ -23,12 +26,12 @@ final actor TokenStorage {
         return try? await KeychainManager.shared.read(.userId)
     }
     
-    func storeTokens(access: String, refresh: String, userId: String? = nil) async {
-        try? await KeychainManager.shared.save(access, for: .accessToken)
-        try? await KeychainManager.shared.save(refresh, for: .refreshToken)
+    func storeTokens(access: String, refresh: String, userId: String? = nil) async throws {
+        try await KeychainManager.shared.save(access, for: .accessToken)
+        try await KeychainManager.shared.save(refresh, for: .refreshToken)
         
         if let userId{
-            try? await KeychainManager.shared.save(userId, for: .userId)
+            try await KeychainManager.shared.save(userId, for: .userId)
         }
     }
     
@@ -37,7 +40,62 @@ final actor TokenStorage {
     }
     
     func hasValidAccessToken() async -> Bool{
-        guard let accessToken = await getAccessToken() else { return false }
+        guard let accessToken = try? await KeychainManager.shared.read(.accessToken) else { return false }
         return !accessToken.isEmpty
+    }
+    
+    func refreshAccessToken() async throws -> String {
+        if let task = refreshTask {
+            return try await task.value
+        }
+        
+        let task = Task<String, Error> {
+            defer { refreshTask = nil }
+            
+            guard let refreshToken = try? await KeychainManager.shared.read(.refreshToken) else {
+                throw NetworkError.refreshTokenExpired
+            }
+            
+            let router = RefreshRequest.refresh(refreshToken: refreshToken)
+            
+            let request = try await router.asUrlRequest()
+            
+            let data: Data
+            let response: URLResponse
+            
+            do{
+                (data, response) = try await URLSession.shared
+                    .data(for: request)
+            }catch{
+                throw NetworkError.networkFailure(error)
+            }
+            
+            guard let http = response as? HTTPURLResponse else{
+                throw NetworkError.invalidResponse
+            }
+            
+            switch http.statusCode{
+            case 200:
+                do{
+                    let response = try JSONDecoder().decode(RefreshDTO.self
+                                                            , from: data)
+                    try await storeTokens(
+                        access: response.accessToken,
+                        refresh: response.refreshToken
+                    )
+                    return response.accessToken
+                }catch{
+                    throw NetworkError.decodingFailed
+                }
+            case 418:
+                await clear()
+                throw NetworkError.refreshTokenExpired
+            default:
+                throw NetworkError.serverError
+            }
+        }
+        
+        self.refreshTask = task
+        return try await task.value
     }
 }
