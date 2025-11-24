a889ec1bf629be699ea6de792b561ff1349dc7b1
rmfls0606 <rmfls0606@naver.com>
2025-11-25 02:59:38 +0900

    feat: 피드 추가 버튼 UI 구성(#13)

--- Contem/Feature/Feed/View/Component/Search/FeedSearchBarView.swift
+++ Contem/Feature/Feed/View/Component/Search/FeedSearchBarView.swift
@@ -16,19 +16,29 @@ struct FeedSearchBarView: View {
     // MARK: - Body
     
     var body: some View {
-        HStack {
-            TextField("", text: $searchText, prompt: Text("브랜드, 상품, 프로필, 태그 등")
+        HStack(spacing: 0){
+            HStack {
+                TextField("", text: $searchText, prompt: Text("브랜드, 상품, 프로필, 태그 등")
+                    .font(.bodyRegular)
+                    .foregroundColor(.gray300)
+                )
                 .font(.bodyRegular)
-                .foregroundColor(.gray300)
-            )
-            .font(.bodyRegular)
+                
+                Spacer()
+            }//: HSTACK
+            .padding(.horizontal(16))
+            .padding(.vertical(12))
+            .background(.gray50)
+            .cornerRadiusSmall()
+            .padding(.horizontal(16))
             
-            Spacer()
-        }
-        .padding(.horizontal(16))
-        .padding(.vertical(12))
-        .background(.gray50)
-        .cornerRadiusSmall()
-        .padding(.horizontal(16))
+            Button {
+                print("피드 생성")
+            } label: {
+                Image(systemName: "camera.fill")
+                    .foregroundStyle(.primary100)
+            }
+            .padding(.trailing, .spacing16)
+        }//: HSTACK
     }
 }
