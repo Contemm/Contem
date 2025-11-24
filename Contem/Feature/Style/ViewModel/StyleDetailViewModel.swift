af0063a896e2e798dc556d71726b62cc6a7e626b
rmfls0606 <rmfls0606@naver.com>
2025-11-25 02:45:22 +0900

    feat: 좋아요 버튼 선택 시 낙관적 방식으로 좋아요 상태 변화 및 마지막 선택 기반 통신 로직 구현(#13)

--- Contem/Feature/Style/ViewModel/StyleDetailViewModel.swift
+++ Contem/Feature/Style/ViewModel/StyleDetailViewModel.swift
@@ -18,11 +18,13 @@ final class StyleDetailViewModel: ViewModelType{
     
     struct Input{
         let appear = PassthroughSubject<Void, Never>()
+        let likebuttonTapped = PassthroughSubject<Void,Never>()
         let commentButtonTapped = PassthroughSubject<String, Never>()
     }
     
     struct Output{
         var style: StyleEntity?
+        var isStyleLiked: Bool = false
         var isLoading: Bool = false
         var errorMessage: String?
         var tags: [Int: [StyleTag]] = [:]
@@ -30,10 +32,16 @@ final class StyleDetailViewModel: ViewModelType{
     
     private let postId: String
     private weak var coordinator: AppCoordinator?
+    private var currentUserId: String? //캐싱된 UserID
+    private let networkLikeTrigger = PassthroughSubject<Void, Never>() //디바운싱용 Subject
 
-    init(postId: String, coordinator: AppCoordinator) {
+    init(postId: String = APIConfig.testPostId, coordinator: AppCoordinator) {
         self.postId = postId
         self.coordinator = coordinator
+        Task{
+            self.currentUserId = await TokenStorage.shared.getUserId()
+        }
+
         transform()
     }
 
@@ -47,6 +55,30 @@ final class StyleDetailViewModel: ViewModelType{
             }
             .store(in: &cancellables)
         
+        input.likebuttonTapped
+            .sink { [weak self] _ in
+                guard let self else { return }
+                self.handleOptimisticLike() //UI 먼저 업데이트
+                self.networkLikeTrigger.send(()) //서버 요청 트리거
+            }
+            .store(in: &cancellables)
+        
+        //디바운싱 체이 -> 서버 요청
+        networkLikeTrigger
+            .debounce(for: .seconds(0.3), scheduler: RunLoop.main)
+            .sink { [weak self] _ in
+                guard let self,
+                      let style = output.style,
+                      let userId = self.currentUserId else { return }
+                
+                Task{
+                    //서버에 보낼 현재 상태는 Model의 상태를 기준으로 함\
+                    let isLiked = style.likes.contains(userId)
+                    await self.postLikeToServer(postId: self.postId, isLiked: isLiked)
+                }
+            }
+            .store(in: &cancellables)
+        
         input.commentButtonTapped
             .withUnretained(self)
             .sink { owner, postId in
@@ -55,6 +87,51 @@ final class StyleDetailViewModel: ViewModelType{
     }
     
     //MARK: - Functions
+    //좋아요 UI 즉시 업데이트(낙관적 업데이트)
+    private func handleOptimisticLike(){
+        guard let userId = currentUserId,
+              var style = output.style else {
+            output.errorMessage = "로그인 후 이용 가능합니다."
+            return
+        }
+        
+        style.toggleLike(userId: userId)
+        
+        output.style = style
+        output.isStyleLiked.toggle()
+    }
+    
+    //롤백 함수
+    private func rollbackLikeState(){
+        guard let userId = currentUserId,
+              var style = output.style else { return }
+        style.toggleLike(userId: userId) //상태 되돌리기
+        output.style = style
+        output.isStyleLiked.toggle()
+    }
+    
+    //좋아요 서버 요청
+    private func postLikeToServer(postId: String, isLiked: Bool) async{
+        do{
+            let response = try await NetworkService.shared.callRequest(router: PostRequest.like(postId: postId, isLiked: isLiked), type: PostLikeDTO.self)
+            
+            self.output.isStyleLiked = response.isLiked
+        }catch let error as NetworkError{
+            self.rollbackLikeState()
+            
+            if case .statusCodeError(let type) = error {
+                if type == .refreshTokenExpired() || type == .forbidden() || type == .unauthorized(){
+                    currentUserId = nil
+                }
+            }
+            
+            output.errorMessage = error.errorDescription
+        }catch{
+            self.rollbackLikeState()
+            output.errorMessage = NetworkError.unknown(error).errorDescription
+        }
+    }
+
     private func preParseAllTags(entity: StyleEntity) {
         let values = [
             entity.value1,
@@ -102,13 +179,21 @@ final class StyleDetailViewModel: ViewModelType{
         Task{
             do{
                 let response = try await NetworkService.shared.callRequest(
-                    router: PostRequest.post(postId: APIConfig.testPostId), //실제로 넘겨 받은 id로 넣기
+                    router: PostRequest.post(postId: postId), //실제로 넘겨 받은 id로 넣기
                     type: PostDTO.self
                 )
                 
                 let entity = response.toEntity()
                 output.style = entity
                 preParseAllTags(entity: entity)
+                
+                let isLiked: Bool
+                if let userId = currentUserId{
+                    isLiked = entity.likes.contains(userId)
+                }else{
+                    isLiked = false
+                }
+                output.isStyleLiked = isLiked
             }catch{
                 output.errorMessage = error.localizedDescription
             }
