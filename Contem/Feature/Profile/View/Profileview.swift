634d8db9e82d439c211b487972054c2e12f49543
rmfls0606 <rmfls0606@naver.com>
2025-11-26 02:48:13 +0900

    feat: 프로필 임시 화면 UI 생성(#13)

---
+++ Contem/Feature/Profile/View/Profileview.swift
@@ -0,0 +1,18 @@
+//
+//  Profileview.swift
+//  Contem
+//
+//  Created by 이상민 on 11/26/25.
+//
+
+import SwiftUI
+
+struct Profileview: View {
+    var body: some View {
+        Text("프로필 화면입니다.")
+    }
+}
+
+#Preview {
+    Profileview()
+}
