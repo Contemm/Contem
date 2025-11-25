ecb32c65fd837ac18df61ce35f0195de3f7cd760
rmfls0606 <rmfls0606@naver.com>
2025-11-26 04:02:06 +0900

    feat: 다른사람 프로필 화면 접근 시 보여줄 데이터 통신 로직 ViewModel에 정의(#13)

---
+++ Contem/Feature/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  ProfileViewModel.swift
+//  Contem
+//
+//  Created by 이상민 on 11/26/25.
+//
+
+import Foundation
+import Combine
+
+final class ProfileViewModel: ViewModelType{
+    
+    //MARK: - Properties
+    var cancellables = Set<AnyCancellable>()
+    
+    var input = Input()
+    @Published var output: Output = Output()
+    
+    struct Input{
+        let appear = PassthroughSubject<Void, Never>()
+    }
+    
+    struct Output{
+        var profile: ProfileEntity?
+        var errorMessage: String?
+    }
+    
+    private let userId: String
+    
+    init(userId: String){
+        self.userId = userId
+        
+        transform()
+    }
+    
+    func transform() {
+        input.appear
+            .sink { [weak self] _ in
+                Task{
+                    await self?.fetchProfile()
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    func fetchProfile() async{
+        Task{
+            do{
+                let response = try await NetworkService.shared.callRequest(router: UserProfileRequest.getOtherProfile(userId: userId), type: OtherProfileDTO.self)
+                
+                let entity = response.toEntity()
+                output.profile = entity
+            }catch let error as NetworkError{
+                output.errorMessage = error.errorDescription
+            }
+        }
+    }
+}
