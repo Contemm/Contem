258533170f3fa7b830ea4987e2241b93ba26b865
rmfls0606 <rmfls0606@naver.com>
2025-11-27 19:48:28 +0900

    feat: 채팅 내역 리스트 조회 API 응답 모델 DTO 정의(#13)

---
+++ Contem/Shared/Network/Chat/DTO/ChatListDTO.swift
@@ -0,0 +1,42 @@
+//
+//  ChatListDTO.swift
+//  Contem
+//
+//  Created by 이상민 on 11/27/25.
+//
+
+import Foundation
+
+struct ChatListDTO: Codable {
+    let data: [ChatResponseDTO]
+}
+
+struct ChatResponseDTO: Codable {
+    let chatId: String
+    let roomId: String
+    let content: String?
+    let createdAt: Date
+    let sender: Sender
+    let files: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case chatId = "chat_id"
+        case roomId = "room_id"
+        case content
+        case createdAt
+        case sender
+        case files
+    }
+
+    struct Sender: Codable {
+        let userId: String
+        let nick: String
+        let profileImage: String?
+
+        enum CodingKeys: String, CodingKey {
+            case userId = "user_id"
+            case nick
+            case profileImage
+        }
+    }
+}
